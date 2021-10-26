9329d6f8f5bddacbc3b01e48790f182af38cb31b
burcuicen <burcuicen2000@gmail.com>
2021-10-26 22:21:30 +0300

    handling express errors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const ejsMate = require('ejs-mate');
+const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
 const methodOverride = require('method-override')
 const path = require('path')
 const Campground = require('./models/campground')
 //const morgan=require("morgan");
-
 
 mongoose.connect('mongodb://localhost:27017/camp-square', {
   useNewUrlParser: true,
@@ -34,42 +34,63 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 //rendering index file to see if the server working correctly
-app.get('/campgrounds', async (req, res) => {
-  const campgrounds = await Campground.find({})
-  res.render('campgrounds/index', { campgrounds })
-})
+app.get(
+  '/campgrounds',
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({})
+    res.render('campgrounds/index', { campgrounds })
+  }),
+)
 //route for new campground page
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new')
 })
 //post request to create new campground
-app.post('/campgrounds', async (req, res) => {
-  const campground = new Campground(req.body.campground)
-  await campground.save()
-  res.redirect(`/campgrounds/${campground._id}`)
-})
+app.post(
+  '/campgrounds',
+  catchAsync(async (req, res, next) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+  }),
+)
 //shows campgrounds by its id on singular page
 
-app.get('/campgrounds/:id', async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
-  res.render('campgrounds/show', { campground })
-})
-app.get('/campgrounds/:id/edit', async (req, res) => {
-  const campground = await Campground.findById(req.params.id)
-  res.render('campgrounds/edit', { campground })
-})
-app.put("/campgrounds/:id", async(req, res)=>{
-  const {id}=req.params;
-  const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground})
-  res.redirect(`/campgrounds/${campground._id}`)
-})
-app.delete("/campgrounds/:id",async(req,res)=>{
-  const {id}=req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect("/campgrounds")
+app.get(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground })
+  }),
+)
+app.get(
+  '/campgrounds/:id/edit',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/edit', { campground })
+  }),
+)
+app.put(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    })
+    res.redirect(`/campgrounds/${campground._id}`)
+  }),
+)
+app.delete(
+  '/campgrounds/:id',
+  catchAsync(async (req, res) => {
+    const { id } = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+  }),
+)
+app.use((err, req, res, next) => {
+  res.send('IT IS AN ERROR! RUN')
 })
 app.listen(3000, () => {
   console.log('server works')
 })
-
-
