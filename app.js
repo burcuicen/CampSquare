5ccefbfff4cafd4c00ce68d537e8eb53c3ea6dfd
burcuicen <burcuicen2000@gmail.com>
2021-10-26 23:24:40 +0300

    adding validation on server side using Joi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
+const Joi = require('joi')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
@@ -50,8 +51,23 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
-    if (!req.body.campground)
-      throw new ExpressError('Invalid Campground Data', 400)
+    // if (!req.body.campground)
+    //   throw new ExpressError('Invalid Campground Data', 400)
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    })
+    const { error } = campgroundSchema.validate(req.body)
+    //console.log(result)
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(',')
+      throw new ExpressError(msg, 400)
+    }
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
