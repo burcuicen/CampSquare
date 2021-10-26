27010ff1c8aa07eac4512955e62c7ffba8fb9ed9
burcuicen <burcuicen2000@gmail.com>
2021-10-26 23:05:53 +0300

    express error handling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const methodOverride = require('method-override')
 const path = require('path')
 const Campground = require('./models/campground')
@@ -49,6 +50,8 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
   '/campgrounds',
   catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
@@ -88,9 +91,15 @@ app.delete(
     res.redirect('/campgrounds')
   }),
 )
-app.use((err, req, res, next) => {
-  res.send('IT IS AN ERROR! RUN')
+app.all('*', (req, res, next) => {
+  next(new ExpressError('Page Not Found', 404))
 })
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err
+  if (!err.message) err.message = 'Something Went Wrong!'
+  res.status(statusCode).render('error', { err })
+})
+
 app.listen(3000, () => {
   console.log('server works')
 })
