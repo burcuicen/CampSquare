ee114c0b28fb62adf34b9312e6c9152603b46bba
burcuicen <burcuicen2000@gmail.com>
2021-10-28 01:20:36 +0300

    middleware to delete reviews

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Review = require('./review')
 const Schema = mongoose.Schema
 
 const CampgroundSchema = new Schema({
@@ -14,4 +15,16 @@ const CampgroundSchema = new Schema({
     },
   ],
 })
+//middleware to delete also reviews that belongs to the campground after campground is deleted
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    })
+  }
+})
+
 module.exports = mongoose.model('Campground', CampgroundSchema)
