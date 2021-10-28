cb636547fd0ba59ec00b0a62eb8cf4fb5c2107cc
burcuicen <burcuicen2000@gmail.com>
2021-10-28 03:19:27 +0300

    fix an error

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
-const { string, required } = require('joi')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const passportLocalMongoose = require('passport-local-mongoose')
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
   email: {
-    type: string,
+    type: String,
     required: true,
     unique: true,
   },
 })
 
-userSchema.plugin(passportLocalMongoose)
+UserSchema.plugin(passportLocalMongoose)
+
 module.exports = mongoose.model('User', UserSchema)
