629295971c1ec52490c63ab54a3258a59e573142
burcuicen <burcuicen2000@gmail.com>
2021-10-30 01:09:59 +0300

    fixing register issue

--- middleware.js
+++ middleware.js
@@ -1,5 +1,7 @@
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
+    //store to users link to redirect after sign in
+    req.session.returnTo = req.originalUrl
     req.flash('error', 'You must sign in to do that!')
     return res.redirect('/login')
   }
--- routes/users.js
+++ routes/users.js
@@ -12,17 +12,19 @@ router.get('/register', (req, res) => {
 })
 router.post(
   '/register',
-  catchAsync(async (req, res) => {
+  catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body
       const user = new User({ email, username })
       const registeredUser = await User.register(user, password)
-      console.log(registeredUser)
-      req.flash('success', 'Welcome')
-      res.redirect('/campgrounds')
+      req.login(registeredUser, (err) => {
+        if (err) return next(err)
+        req.flash('success', 'Welcome!')
+        res.redirect('/campgrounds')
+      })
     } catch (e) {
       req.flash('error', e.message)
-      res.redirect('/register')
+      res.redirect('register')
     }
   }),
 )
@@ -36,8 +38,10 @@ router.post(
     failureRedirect: '/login',
   }),
   (req, res) => {
-    req.flash('success', 'Welcome Back!!')
-    res.redirect('/campgrounds')
+    req.flash('success', `Welcome Back ${req.user.username}`)
+    const redirectUrl = req.session.returnTo || '/campgrounds'
+    delete req.session.returnTo
+    res.redirect(redirectUrl)
   },
 )
 router.get('/logout', (req, res) => {
