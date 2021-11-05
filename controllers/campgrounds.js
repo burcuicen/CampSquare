419a1a4c8c390f7d4835484c7f01094dbbad9b87
burcuicen <burcuicen2000@gmail.com>
2021-11-05 03:26:21 +0300

    backend of displaying maps for every campground

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,8 +1,13 @@
 const Campground = require('../models/campground')
-const { cloudinary } = require('../cloudinary')
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+const { cloudinary } = require('../cloudinary');
+const campground = require('../models/campground');
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({})
+  console.log(campground.images)
   res.render('campgrounds/index', { campgrounds })
 }
 
@@ -11,7 +16,13 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+}).send()
+
   const campground = new Campground(req.body.campground)
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((f) => ({
     url: f.path,
     filename: f.filename,
