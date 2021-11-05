432cc32cf28b04c2dc1e73298385eec05cf892de
burcuicen <burcuicen2000@gmail.com>
2021-11-05 18:54:08 +0300

    adding popup markup

--- models/campground.js
+++ models/campground.js
@@ -9,6 +9,8 @@ const ImageSchema = new Schema({
 ImageSchema.virtual('thumbnail').get(function () {
   return this.url.replace('/upload', '/upload/w_200')
 })
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
   title: String,
   images: [ImageSchema],
@@ -36,7 +38,13 @@ const CampgroundSchema = new Schema({
       ref: 'Review',
     },
   ],
-})
+},opts)
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+  return `
+  <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+  <p>${this.location}</p>`
+});
+
 //middleware to delete also reviews that belongs to the campground after campground is deleted
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
